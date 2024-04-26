8012a08ec75e07a6206dfc87bb28e91597e5afd8
ouendinga <ouendinga@gmail.com>
2024-04-26 15:13:51 +0200

    Add list language_code_text. Check languages in search_translation. Enable search_translations_config

--- search_translations.py
+++ search_translations.py
@@ -7,9 +7,8 @@ import os
 
 from googletrans import Translator
 
-languages_text = ["Spanish", "English"]
-languages_file = ["es_ES", "en_US"]
 config = {}
+language_code_text = {}
 
 translator = None
 
@@ -22,14 +21,14 @@ def append_to_po_file(file_path, variable, translation, check_translation=True):
     with open(file_path, "a") as output_file:
         output_file.write("msgid \""+variable+"\"\n")
         output_file.write("msgstr \""+translation+"\"\n")
-        print(f"The variable (\033[92m{variable}\033[0m) has been added to the file")
+        print(f"The variable (\033[92m{variable}\033[0m) has been added to the file (\033[92m{file_path}\033[0m)")
 
 def check_translation_po_file(file_path, variable):
     try:
         with open(file_path, "r") as input_file:
             for line in input_file:
                 if "msgid \""+variable+"\"\n" in line:
-                    print(f"The variable (\033[33m{variable}\033[0m) exists, nothing to do")
+                    print(f"The variable (\033[33m{variable}\033[0m) exists in file (\033[92m{file_path}\033[0m), nothing to do")
                     return True  # The variable exists, nothing to do
         return False
     except FileNotFoundError:
@@ -55,6 +54,7 @@ def get_hash_variables(line):
     return result
 
 def search_php_files(directory_search, translate):
+    print("Searching translations in \033[93m" + directory_search+"\033[0m")
     for root, dirs, files in os.walk(directory_search):
         for file in files:
             if file.endswith(".php"):
@@ -74,19 +74,22 @@ def search_php_files(directory_search, translate):
                                     domain_variable = "__(" + variable + ", '"+config["domain"]+"')"
                                     line = line.replace("__("+hash_variable+")", domain_variable)
                                     variable_without_quotes = variable[1:-1]
-                                    for index, language_file in enumerate(languages_file):
-                                        path_po_file = config["path_translations"] + "/" + language_file + ".po"
+                                    for language_code in language_code_text.keys():
+                                        if language_code not in config["languages_code_translate"]:
+                                            continue
+                                        print("\033[93m"+language_code_text[language_code]+"\033[0m")
+                                        path_po_file = config["path_translations"] + "/" + language_code + ".po"
                                         translation = ""
                                         if translate:
                                             if not check_translation_po_file(path_po_file, variable_without_quotes):
                                                 # Se han de quitar el escapar las comillas dobles
                                                 variable_to_translate = variable_without_quotes.replace("\\", "")
                                                 print("To translate: \033[95m"+variable_to_translate+"\033[0m")
-                                                translation = get_translation(variable_to_translate,"ca",language_file[:2])
+                                                translation = get_translation(variable_to_translate,"ca",language_code[:2])
                                                 if translation is not None:
                                                     # Volver a escapar las comillas dobles para el .po
                                                     translation = translation.replace('"', '\\"')
-                                                    print(languages_text[index]+" translation: \033[96m"+translation+"\033[0m")
+                                                    print(language_code_text[language_code]+" translation: \033[96m"+translation+"\033[0m")
                                         # Append the variable to the .po file
                                         append_to_po_file(path_po_file, variable_without_quotes, translation)
                         f.write(line)
@@ -95,23 +98,48 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Search translations in PHP files')
     parser.add_argument('--path_search', default=".", help='Directory to search translations')
     parser.add_argument('--path_translations', default=".", help='Directory save translations')
+    parser.add_argument('--domain', default="translation", help='Domain of the translations')
+    parser.add_argument('--translate', action='store_true', help='Translate variables')
     parser.add_argument('--rem_old_translations', default=False, help='Remove translations that do not exist in the code')
     parser.add_argument('--generate_mo', default=False, help='Generate the .mo files')
-    parser.add_argument('--domain', default="translation", help='Domain of the translations')
     parser.add_argument('--transform_lowercase', default=False, help='Transform string translate to lowercase')
-    parser.add_argument('--translate', action='store_true', help='Translate variables')
+    parser.add_argument('--languages_code_translate', default=[], help='Languages code to translate')
+    parser.add_argument('--file_config', default=False, help='File config params. If is set, the other params are ignored')
+    args = parser.parse_args()
+
+    # Load the config params
+    if not args.file_config:
+        config["path_search"] = args.path_search
+        config["path_translations"] = args.path_translations
+        config["domain"] = args.domain
+        config["translate"] = args.translate
+        config["rem_old_translations"] = args.rem_old_translations
+        config["generate_mo"] = args.generate_mo
+        config["transform_lowercase"] = args.transform_lowercase
+        config["languages_code_translate"] = args.languages_code_translate
+    else:
+        with open(args.file_config, "r") as file:
+            config = json.load(file)
+
+    # print(f"Config params: {config}")
+
+    # Load the language code and text available
+    with open("language_code_text.json", "r") as file:
+        language_code_text = json.load(file)
 
     if config["translate"]:
+        # Init the translator
         translator = Translator()
 
-    print("Searching translations in \033[93m" + config["path_search"]+"\033[0m")
     search_php_files(config["path_search"], config["translate"])
 
     if (config["translate"]):
 
         # Generate the .mo files
         print("Generating .mo files")
-        for language_file in languages_file:
-            os.system("msgfmt "+config["path_translations"] + "/" + language_file + ".po"+" -o "+config["path_translations"] + "/" + language_file + ".mo")
+        for language_code in language_code_text.keys():
+            if language_code not in config["languages_code_translate"]:
+                continue
+            os.system("msgfmt "+config["path_translations"] + "/" + language_code + ".po"+" -o "+config["path_translations"] + "/" + language_code + ".mo")
 
     print("Done!")
